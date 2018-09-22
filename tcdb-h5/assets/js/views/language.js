0bb310cb8deab35113c6483f02dadeca5b36789b
ct <978744151@qq.com>
2018-09-22 13:07:06 +0800

    new tcdb

---
+++ tcdb-h5/assets/js/views/language.js
@@ -0,0 +1,43 @@
+define(
+    [
+        'zepto', 'jquery', 'underscore', 'backbone', 'urlGroup',
+        'swiper', 'echo', 'app/api', 'app/basket',
+        'app/utils', 'app/scroll',
+        'text!templates/language.html'
+    ],
+
+    function($, Jquery, _, Backbone, UrlGroup, Swiper, echo, Api, basket, utils, scroll,
+             language) {
+
+        var $page = $("#main-language");
+        var languageView = Backbone.View.extend({
+            el: $page,
+            render: function () {
+                utils.showPage($page, function () {
+                    $page.empty().append(language);
+                    languageBottom()
+                });
+            },
+
+
+        })
+        var languageBottom = function () {
+            $('.languageBottom div').on('tap', function () {
+                console.log($(this));
+                var $this = $(this);
+                $this.addClass('active').siblings().removeClass('active')
+            })
+            $('.languageGo').on('tap', function () {
+                var english = $(".English"),
+                    tai = $('.tai')
+                if(english.hasClass('active')){
+                    localStorage.setItem('lan',1)
+                }else{
+                    localStorage.setItem('lan',2)
+                }
+                window.location.hash = 'main'
+
+            })
+        }
+        return languageView;
+    });
